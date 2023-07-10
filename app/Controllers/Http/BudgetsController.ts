87e05dc4e8f9e0b38b09767223826c0ae87ff214
davidhh98 <136001294+dhenao-i4digital@users.noreply.github.com>
2023-07-10 18:01:17 -0500

    se agrega crud para edtiar y crear pospre

--- app/Controllers/Http/BudgetsController.ts
+++ app/Controllers/Http/BudgetsController.ts
@@ -2,6 +2,7 @@ import type { HttpContextContract } from "@ioc:Adonis/Core/HttpContext";
 import BudgetsProvider from "@ioc:core.BudgetsProvider";
 import { EResponseCodes } from "App/Constants/ResponseCodesEnum";
 import { ApiResponse } from "App/Utils/ApiResponses";
+import { IFilterBudgets } from "App/Interfaces/BudgetsInterfaces";
 import BudgetsValidator from "App/Validators/BudgetsValidator";
 
 export default class BudgetsController {
@@ -16,38 +17,60 @@ export default class BudgetsController {
     }
   }
 
+  //    public async deleteBudgets({ request, response }: HttpContextContract) {
+  //     try {
+  //       const { id } = request.params();
+  //       return response.send(await BudgetsProvider.deleteBudgets(id));
+  //     } catch (err) {
+  //       return response.badRequest(
+  //         new ApiResponse(null, EResponseCodes.FAIL, String(err))
+  //       );
+  //     }
+  //   }
 
-//    public async deleteBudgets({ request, response }: HttpContextContract) {
-//     try {
-//       const { id } = request.params();
-//       return response.send(await BudgetsProvider.deleteBudgets(id));
-//     } catch (err) {
-//       return response.badRequest(
-//         new ApiResponse(null, EResponseCodes.FAIL, String(err))
-//       );
-//     }
-//   }
+  //   public async updateBudgets({ request, response }: HttpContextContract) {
+  //     try {
+  //       const { id } = request.params();
+  //       const data = await request.validate(BudgetsValidator);
+  //       return response.send(await BudgetsProvider.updateBudgets(data, id));
+  //     } catch (err) {
+  //       return response.badRequest(
+  //         new ApiResponse(null, EResponseCodes.FAIL, String(err))
+  //       );
+  //     }
+  //   }
+  
+     public async createBudgets({ request, response }: HttpContextContract) {
+      try {
+        const data = await request.validate(BudgetsValidator);
+        return response.send(await BudgetsProvider.createBudgets(data));
+      } catch (err) {
+        return response.badRequest(
+          new ApiResponse(null, EResponseCodes.FAIL, String(err))
+        );
+      }
+    }
 
-//   public async updateBudgets({ request, response }: HttpContextContract) {
-//     try {
-//       const { id } = request.params();
-//       const data = await request.validate(BudgetsValidator);
-//       return response.send(await BudgetsProvider.updateBudgets(data, id));
-//     } catch (err) {
-//       return response.badRequest(
-//         new ApiResponse(null, EResponseCodes.FAIL, String(err))
-//       );
-//     }
-//   }
+  public async getBudgetsPaginated({ request, response }: HttpContextContract) {
+    try {
+      const data = request.body() as IFilterBudgets;
+      return response.send(await BudgetsProvider.getBudgetsPaginated(data));
+    } catch (err) {
+      return response.badRequest(
+        new ApiResponse(null, EResponseCodes.FAIL, String(err))
+      );
+    }
+  }
 
-//   public async createBudgets({ request, response }: HttpContextContract) {
-//     try {
-//       const data = await request.validate(BudgetsValidator);
-//       return response.send(await BudgetsProvider.createBudgets(data));
-//     } catch (err) {
-//       return response.badRequest(
-//         new ApiResponse(null, EResponseCodes.FAIL, String(err))
-//       );
-//     }
-//   }
+  public async updateBudgets({ request, response }: HttpContextContract) {
+    try {
+      const { id } = request.params();
+      const data = await request.validate(BudgetsValidator);
+      return response.send(await BudgetsProvider.updateBudgets(data, id));
+    } catch (err) {
+      return response.badRequest(
+        new ApiResponse(null, EResponseCodes.FAIL, String(err))
+      );
+    }
+  }
 }
