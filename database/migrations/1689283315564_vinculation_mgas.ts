d80bce968bf1674acd97d64f2994a788b371eb77
Daniel Flores <dflores@i4digital.com>
2023-07-19 13:12:25 -0500

    ajustes

--- database/migrations/1689283315564_vinculation_mgas.ts
+++ database/migrations/1689283315564_vinculation_mgas.ts
@@ -1,38 +1,44 @@
-import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+import BaseSchema from "@ioc:Adonis/Lucid/Schema";
 
 export default class extends BaseSchema {
-  protected tableName = 'VMG_VINCULACIONES_MGA'
+  protected tableName = "VMG_VINCULACIONES_MGA";
 
-  public async up () {
+  public async up() {
     this.schema.createTable(this.tableName, (table) => {
-        table.comment("Tabla que asocia las actividades MGA con la posicion presupuestaria origen");
-        table.increments("VMG_CODIGO")
-        .primary()
-        .comment("Llave primaria");
-        table
+      table.comment(
+        "Tabla que asocia las actividades MGA con la posicion presupuestaria origen"
+      );
+      table.increments("VMG_CODIGO").primary().comment("Llave primaria");
+      table
         .integer("VMG_CODPPR_POSICION_PRESUPUESTAL")
         .notNullable()
+        .unsigned()
         .references("PPR_CODIGO")
         .inTable("PPR_POSICIONES_PRESUPUESTARIAS")
-        .comment("Codigo de la posion presupuestal (FK PPR_POSICIONES_PRESUPUESTARIAS)");
-        table
+        .comment(
+          "Codigo de la posion presupuestal (FK PPR_POSICIONES_PRESUPUESTARIAS)"
+        );
+      table
         .integer("VMG_CODMGA_METODOLOGIA_GENERAL")
         .notNullable()
+        .unsigned()
         .references("MGA_CODIGO")
         .inTable("MGA_ACTIVDADES_DETALLADAS_MGA")
-        .comment("Codigo de la metodologia ajustada (FK MGA_ACTIVDAD_DETALLADA_MGA)");
-        table
-        .integer("VMG_USUARIO_CREO",15)
+        .comment(
+          "Codigo de la metodologia ajustada (FK MGA_ACTIVDAD_DETALLADA_MGA)"
+        );
+      table
+        .integer("VMG_USUARIO_CREO", 15)
         .notNullable()
         .comment("Numero del documento del usuario que creo el registro");
-        table
+      table
         .timestamp("VMG_FECHA_CREO")
         .notNullable()
         .comment("Fecha y hora de creación del registro");
-    })
+    });
   }
 
-  public async down () {
-    this.schema.dropTable(this.tableName)
+  public async down() {
+    this.schema.dropTable(this.tableName);
   }
 }
--- database/migrations/1689285055790_position_budgets.ts
+++ database/migrations/1689285055790_position_budgets.ts
@@ -1,58 +1,59 @@
-import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+import BaseSchema from "@ioc:Adonis/Lucid/Schema";
 
 export default class extends BaseSchema {
-  protected tableName = 'PPS_POSICIONES_PRESUPUESTARIAS_SAPIENCIA'
+  protected tableName = "PPS_POSICIONES_PRESUPUESTARIAS_SAPIENCIA";
 
-  public async up () {
+  public async up() {
     this.schema.createTable(this.tableName, (table) => {
       table.comment("Tabla que almacena los tipos presupuestos");
-      table.increments("PPS_CODIGO")
-      .primary()
-      .comment("Llave primaria");
+      table.increments("PPS_CODIGO").primary().comment("Llave primaria");
       table
-      .string("PPS_CODIGO_REFERENCIA",20)
-      .notNullable()
-      .comment("Codigo de referencia ingresado");
+        .string("PPS_CODIGO_REFERENCIA", 20)
+        .notNullable()
+        .comment("Codigo de referencia ingresado");
       table
-      .integer("PPS_CODPPR_POSICION_PRESUPUESTAL")
-      .notNullable()
-      .references("PPR_CODIGO")
-      .inTable("PPR_POSICIONES_PRESUPUESTARIAS")
-      .comment("Codigo de la posion presupuestal (FK PPR_POSICIONES_PRESUPUESTARIAS)");
+        .integer("PPS_CODPPR_POSICION_PRESUPUESTAL")
+        .notNullable()
+        .unsigned()
+        .comment(
+          "Codigo de la posion presupuestal (FK PPR_POSICIONES_PRESUPUESTARIAS)"
+        );
+      table.integer("PPS_EJERCICIO").notNullable().comment("Año del ejercicio");
+      table.string("PPS_DESCRIPCION", 500).notNullable().comment("Descripcion");
       table
-      .integer("PPS_EJERCICIO")
-      .notNullable()
-      .comment("Año del ejercicio");
+        .integer("PPS_CONSECUTIVO")
+        .notNullable()
+        .comment("Numero consecutivo");
       table
-      .string("PPS_DESCRIPCION",500)
-      .notNullable()
-      .comment("Descripcion");
+        .string("PPS_ASIGNADO_A", 20)
+        .notNullable()
+        .comment("Codigo donde se Asignara");
       table
-      .integer("PPS_CONSECUTIVO")
-      .notNullable()
-      .comment("Numero consecutivo");
+        .string("PPS_USUARIO_MODIFICO", 15)
+        .comment(
+          "Número del documento del último usuario que hizo una modificación"
+        );
       table
-      .string("PPS_ASIGNADO_A", 20)
-      .notNullable()
-      .comment("Codigo donde se Asignara");
+        .timestamp("PPS_FECHA_MODIFICO")
+        .comment("Fecha y hora de la última modificación");
       table
-      .string("PPS_USUARIO_MODIFICO", 15)
-      .comment("Número del documento del último usuario que hizo una modificación");
+        .string("PPS_USUARIO_CREO", 15)
+        .notNullable()
+        .comment("Número del documento del usuario que creó el registro");
       table
-      .timestamp("PPS_FECHA_MODIFICO")
-      .comment("Fecha y hora de la última modificación");
+        .timestamp("PPS_FECHA_CREO")
+        .notNullable()
+        .comment("Fecha y hora de creación del registro");
+
+      // Llaves foraneas
       table
-      .string("PPS_USUARIO_CREO", 15)
-      .notNullable()
-      .comment("Número del documento del usuario que creó el registro");
-      table
-      .timestamp("PPS_FECHA_CREO")
-      .notNullable()
-      .comment("Fecha y hora de creación del registro");
-    })
+        .foreign("PPS_CODPPR_POSICION_PRESUPUESTAL", "FK_CODPPS_CODPPR")
+        .references("PPR_CODIGO")
+        .inTable("PPR_POSICIONES_PRESUPUESTARIAS");
+    });
   }
 
-  public async down () {
-    this.schema.dropTable(this.tableName)
+  public async down() {
+    this.schema.dropTable(this.tableName);
   }
 }
