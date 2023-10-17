0e17334b471d63f718efa89b67a4247ad566ab41
leiner mena <lmena@i4digital.com>
2023-10-17 15:51:35 -0500

    Creacion de modelos para CDP

---
+++ app/Models/CertificateBudgetAvailability.ts
@@ -0,0 +1,23 @@
+import { BaseModel, column } from "@ioc:Adonis/Lucid/Orm";
+import { DateTime } from "luxon";
+
+export default class CdpCertificadoDisponibilidadPresupuestal extends BaseModel {
+  public static table = "CDP_CERTIFICADO_DISPONIBILIDAD_PRESUPUESTAL";
+
+  @column({ isPrimary: true, columnName: "CDP_CODIGO", serializeAs: "id" })
+  public id: number;
+
+  @column.dateTime({
+    autoCreate: true,
+    columnName: "CDP_FECHA",
+    serializeAs: "date",
+  })
+  public date: DateTime;
+
+  @column({ columnName: "CDP_OBJETO_CONTRACTUAL", serializeAs: "contractObject" })
+  public contractObject: string;
+
+  @column({ columnName: "CDP_CONSECUTIVO", serializeAs: "consecutive" })
+  public consecutive: number;
+
+}
---
+++ app/Models/IcdAmountsCdp.ts
@@ -0,0 +1,36 @@
+import { BaseModel, HasOne, column, hasOne } from '@ioc:Adonis/Lucid/Orm';
+import CertificateBudgetAvailability from './CertificateBudgetAvailability';
+import BudgetsRoutes from './BudgetsRoutes';
+
+export default class IcdAmountsCdp extends BaseModel {
+  public static table = 'ICD_IMPORTES_CDP';
+
+  @column({ isPrimary: true, columnName: 'ICD_CODIGO', serializeAs: 'id' })
+  public id: number;
+
+  @column({ columnName: 'ICD_CODCDP', serializeAs: 'cdpCode' })
+  public cdpCode: number;
+
+  @column({ columnName: 'ICD_CODRPP_RUTA_PRESUPUESTAL', serializeAs: 'idRppCode' })
+  public rppCode: number;
+
+  @column({ columnName: 'ICD_POSICION', serializeAs: 'cdpPosition' })
+  public position: number;
+
+  @column({ columnName: 'ICD_VALOR', serializeAs: 'amount' })
+  public value: number;
+
+  @hasOne(() => CertificateBudgetAvailability, {
+    localKey: 'id',
+    foreignKey: 'ICD_CODCDP',
+    serializeAs: "certificateBudgetAvailability",
+  })
+  public certificateBudgetAvailability: HasOne<typeof CertificateBudgetAvailability>; /* Preguntar si esta relacion esta bien */
+
+  @hasOne(() => BudgetsRoutes, {
+    localKey: 'idRppCode',
+    foreignKey: 'id',
+    serializeAs: "budgetRoute",
+  })
+  public budgetRoute: HasOne<typeof BudgetsRoutes>; /* Preguntar si esta relacion esta bien */
+}
