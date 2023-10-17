19355eb479e97b77e9d27922c3a331b74d13716a
leiner mena <lmena@i4digital.com>
2023-10-17 14:32:06 -0500

    Migraciones CDP

---
+++ database/migrations/1697564001416_create_cdp_certificado_disponibilidad_presupuestal_tables.ts
@@ -0,0 +1,18 @@
+import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+
+export default class extends BaseSchema {
+  protected tableName = 'CDP_CERTIFICADO_DISPONIBILIDAD_PRESUPUESTAL'
+
+  public async up () {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('CDP_CODIGO').primary()
+      table.date('CDP_FECHA').notNullable()
+      table.string('CDP_OBJETO_CONTRACTUAL', 5000).notNullable()
+      table.integer('CDP_CONSECUTIVO').notNullable()
+    })
+  }
+
+  public async down () {
+    this.schema.dropTable(this.tableName)
+  }
+}
