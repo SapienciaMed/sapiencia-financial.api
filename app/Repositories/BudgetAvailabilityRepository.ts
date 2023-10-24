b0fefaf071c86a70ab691722efe248b86812a89a
leiner mena <lmena@i4digital.com>
2023-10-24 14:46:23 -0500

    Ajustes y creacion de enpoint budget routes

--- app/Repositories/BudgetAvailabilityRepository.ts
+++ app/Repositories/BudgetAvailabilityRepository.ts
@@ -97,7 +97,7 @@ export default class BudgetAvailabilityRepository
         .where('CDP_OBJETO_CONTRACTUAL', 'LIKE', `%${contractObject}%`)
         .preload('amounts');
 
-    const cdps = results.map(result => result.toJSON());
+    const cdps =   results.map(result => result.toJSON());
     return cdps;
 }
 
