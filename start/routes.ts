2101bd9a14730fd68156848c623939b7cdb2fa6a
CristianWhitesoft <cristian.badillo@whitesoft.com.au>
2023-11-11 13:59:37 -0500

    middleware auth

--- start/routes.ts
+++ start/routes.ts
@@ -37,13 +37,13 @@ Route.group(() => {
   Route.post("/create", "FundsController.createFund");
   Route.put("/update/:id", "FundsController.updateFund");
   Route.get("/get-all", "FundsController.getAllFunds");
-}).prefix("/api/v1/funds")
+})
+  .prefix("/api/v1/funds")
   .middleware("auth");
 
 Route.group(() => {
   Route.post("/get-paginated", "PagPagosController.getPagosPaginated");
   Route.post("/create", "PagPagosController.processDocument");
-
 }).prefix("/api/v1/pag-pagos");
 
 Route.group(() => {
@@ -52,50 +52,105 @@ Route.group(() => {
   Route.post("/create", "BudgetsController.createBudgets");
   Route.put("/update/:id", "BudgetsController.updateBudgets");
   Route.get("/get-all", "BudgetsController.getAllBudgets");
-}).prefix("/api/v1/budgets")
+})
+  .prefix("/api/v1/budgets")
   .middleware("auth");
 
 Route.group(() => {
   Route.get("/get-all", "EntitiesController.getEntities");
-}).prefix("/api/v1/entities")
+})
+  .prefix("/api/v1/entities")
   .middleware("auth");
 
 Route.group(() => {
-  Route.post("/get-paginated","PosPreSapienciaController.getPosPreSapienciaPaginated");
+  Route.post(
+    "/get-paginated",
+    "PosPreSapienciaController.getPosPreSapienciaPaginated"
+  );
   Route.put("/update/:id", "PosPreSapienciaController.updatePosPreSapiencia");
   Route.get("/get-all", "PosPreSapienciaController.getAllPosPreSapiencia");
-  Route.get("/get-posprevinculation-by-id/:id", "PosPreSapienciaController.getPosPreSapienciaById");
-  Route.post("/get-list-pospresap-vinculation-paginated", "PosPreSapienciaController.getListPosPreSapVinculationPaginated");
-  Route.post("/create-pospresap-vinculation", "PosPreSapienciaController.createPosPreSapVinculation");
-  Route.post("/update-pospresap-vinculation/:id", "PosPreSapienciaController.updatePosPreSapVinculation");
-}).prefix("/api/v1/pospre-sapiencia")
+  Route.get(
+    "/get-posprevinculation-by-id/:id",
+    "PosPreSapienciaController.getPosPreSapienciaById"
+  );
+  Route.post(
+    "/get-list-pospresap-vinculation-paginated",
+    "PosPreSapienciaController.getListPosPreSapVinculationPaginated"
+  );
+  Route.post(
+    "/create-pospresap-vinculation",
+    "PosPreSapienciaController.createPosPreSapVinculation"
+  );
+  Route.post(
+    "/update-pospresap-vinculation/:id",
+    "PosPreSapienciaController.updatePosPreSapVinculation"
+  );
+})
+  .prefix("/api/v1/pospre-sapiencia")
   .middleware("auth");
 
 Route.group(() => {
   Route.get("/get-by-id/:id", "VinculationMGAController.getVinculationMGAById");
-  Route.post("/get-paginated","VinculationMGAController.getVinculationMGAPaginated");
-  Route.post("/get-detailed-activities-api-planning", "VinculationMGAController.getDetailedActivitiesV2");
-  Route.post("/create-vinculation-api-planning", "VinculationMGAController.createVinculationWithPlanningV2");
-  Route.post("/get-detailed-activities-api-planning-nouseonpospre/:pospreorgid", "VinculationMGAController.getDetailedActivitiesNoUseOnPosPre");
-  Route.post("/get-detailed-activities-api-planning-yesuseonpospre/:pospreorgid", "VinculationMGAController.getDetailedActivitiesYesUseOnPosPre");
-  Route.get("/get-detailed-activities-api-planning-by-id/:id", "VinculationMGAController.getVinculationDetailedActivitiesV2ById");
-  Route.post("/update-vinculation-multiple","VinculationMGAController.updateMultipleVinculation");
-}).prefix("/api/v1/vinculation-mga")
+  Route.post(
+    "/get-paginated",
+    "VinculationMGAController.getVinculationMGAPaginated"
+  );
+  Route.post(
+    "/get-detailed-activities-api-planning",
+    "VinculationMGAController.getDetailedActivitiesV2"
+  );
+  Route.post(
+    "/create-vinculation-api-planning",
+    "VinculationMGAController.createVinculationWithPlanningV2"
+  );
+  Route.post(
+    "/get-detailed-activities-api-planning-nouseonpospre/:pospreorgid",
+    "VinculationMGAController.getDetailedActivitiesNoUseOnPosPre"
+  );
+  Route.post(
+    "/get-detailed-activities-api-planning-yesuseonpospre/:pospreorgid",
+    "VinculationMGAController.getDetailedActivitiesYesUseOnPosPre"
+  );
+  Route.get(
+    "/get-detailed-activities-api-planning-by-id/:id",
+    "VinculationMGAController.getVinculationDetailedActivitiesV2ById"
+  );
+  Route.post(
+    "/update-vinculation-multiple",
+    "VinculationMGAController.updateMultipleVinculation"
+  );
+})
+  .prefix("/api/v1/vinculation-mga")
   .middleware("auth");
 
 Route.group(() => {
   Route.get("/get-by-id/:id", "FunctionalAreaController.getFunctionalAreaById");
-  Route.post("/get-paginated","FunctionalAreaController.getFunctionalAreaPaginated");
+  Route.post(
+    "/get-paginated",
+    "FunctionalAreaController.getFunctionalAreaPaginated"
+  );
   Route.post("/create", "FunctionalAreaController.createFunctionalArea");
   Route.put("/update/:id", "FunctionalAreaController.updateFunctionalArea");
   Route.get("/get-all", "FunctionalAreaController.getAllFunctionalAreas");
-  Route.post("/link/create","FunctionalAreaController.createProjectFunctionalArea");
-  Route.post("/link/update","FunctionalAreaController.updateProjectFunctionalArea");
-  Route.delete("/link/delete/:id","FunctionalAreaController.deleteProjectFunctionalArea");
-  Route.post("/link/get-paginated","FunctionalAreaController.getProjectFunctionalAreaPaginated");
-}).prefix("/api/v1/functional-area")
+  Route.post(
+    "/link/create",
+    "FunctionalAreaController.createProjectFunctionalArea"
+  );
+  Route.post(
+    "/link/update",
+    "FunctionalAreaController.updateProjectFunctionalArea"
+  );
+  Route.delete(
+    "/link/delete/:id",
+    "FunctionalAreaController.deleteProjectFunctionalArea"
+  );
+  Route.post(
+    "/link/get-paginated",
+    "FunctionalAreaController.getProjectFunctionalAreaPaginated"
+  );
+})
+  .prefix("/api/v1/functional-area")
   .middleware("auth");
-
 
 Route.group(() => {
   Route.post("/get-paginated", "ProjectsController.getProjectsPaginated");
@@ -104,18 +159,27 @@ Route.group(() => {
     "/get-unrelated-projects",
     "ProjectsController.getUnrelatedProjects"
   );
-}).prefix("/api/v1/projects").middleware("auth");
-
+})
+  .prefix("/api/v1/projects")
+  .middleware("auth");
 
 Route.group(() => {
-  Route.get("/get-by-id/:id","ManagementCenterController.getManagementCenterById");
-  Route.post("/get-paginated","ManagementCenterController.getManagementCenterPaginated");
-}).prefix("/api/v1/management-center")
+  Route.get(
+    "/get-by-id/:id",
+    "ManagementCenterController.getManagementCenterById"
+  );
+  Route.post(
+    "/get-paginated",
+    "ManagementCenterController.getManagementCenterPaginated"
+  );
+})
+  .prefix("/api/v1/management-center")
   .middleware("auth");
 
 Route.group(() => {
   Route.get("/get-all", "TypesTransfersController.getTypeTransfers");
-}).prefix("/api/v1/type-transfers")
+})
+  .prefix("/api/v1/type-transfers")
   .middleware("auth");
 
 Route.group(() => {
@@ -130,18 +194,28 @@ Route.group(() => {
   );
   Route.post("/create", "BudgetsRoutesController.createBudgetsRoutes");
   Route.put("/update/:id", "BudgetsRoutesController.updateBudgetsRoutes");
-}).prefix("/api/v1/budget-routes")
+})
+  .prefix("/api/v1/budget-routes")
   .middleware("auth");
 
 Route.group(() => {
   Route.post("/get-paginated", "AdditionsController.getAdditionsPaginated");
   Route.post("/get-funds", "AdditionsController.getFundsList");
   Route.post("/get-pospre", "AdditionsController.getPosPreList");
-  Route.post("/get-pospre-sapiencia","AdditionsController.getPosPreSapienciaList");
+  Route.post(
+    "/get-pospre-sapiencia",
+    "AdditionsController.getPosPreSapienciaList"
+  );
   Route.post("/save-data", "AdditionsController.executeCreateAdditions"); //Como acción de guardado
   Route.post("/create", "AdditionsController.createAdditions"); //Como acción validación
-  Route.get("/get-actadmin-district","AdditionsController.getAllAdditionsByDistrict");
-  Route.get("/get-actadmin-sapiencia","AdditionsController.getAllAdditionsBySapiencia");
+  Route.get(
+    "/get-actadmin-district",
+    "AdditionsController.getAllAdditionsByDistrict"
+  );
+  Route.get(
+    "/get-actadmin-sapiencia",
+    "AdditionsController.getAllAdditionsBySapiencia"
+  );
   Route.get("/get-by-id/:id", "AdditionsController.getAdditionById");
   Route.post("/update/:id", "AdditionsController.updateAdditionWithMov"); //Como acción de validación
   Route.post(
@@ -150,29 +224,54 @@ Route.group(() => {
   ); //Como acción de guardado/actualizado
   Route.post("/get-info-filter", "AdditionsController.budgetCdp"); //Como acción de guardado/actualizado
   ("AdditionsController.executeUpdateAdditionWithMov");
-}).prefix("/api/v1/additions");
+})
+  .prefix("/api/v1/additions")
+  .middleware("auth");
 
 Route.group(() => {
   Route.post("/get-paginated", "TransfersController.getTransfersPaginated");
   Route.post("/get-funds", "TransfersController.getFundsList");
   Route.post("/get-pospre", "TransfersController.getPosPreList");
-  Route.post( "/get-pospre-sapiencia", "TransfersController.getPosPreSapienciaList");
+  Route.post(
+    "/get-pospre-sapiencia",
+    "TransfersController.getPosPreSapienciaList"
+  );
   Route.post("/save-data", "TransfersController.executeCreateTransfers"); //Como acción de guardado
   Route.post("/create", "TransfersController.createTransfers"); //Como acción validación
-  Route.get("/get-actadmin-district","TransfersController.getAllTransfersByDistrict");
-  Route.get("/get-actadmin-sapiencia","TransfersController.getAllTransfersBySapiencia");
+  Route.get(
+    "/get-actadmin-district",
+    "TransfersController.getAllTransfersByDistrict"
+  );
+  Route.get(
+    "/get-actadmin-sapiencia",
+    "TransfersController.getAllTransfersBySapiencia"
+  );
   Route.get("/get-by-id/:id", "TransfersController.getTransferById");
   Route.post("/update/:id", "TransfersController.updateTransferWithMov"); //Como acción de validación
-  Route.post("/update-save/:id","TransfersController.executeUpdateTransferWithMov");
-}).prefix("/api/v1/transfers")
+  Route.post(
+    "/update-save/:id",
+    "TransfersController.executeUpdateTransferWithMov"
+  );
+})
+  .prefix("/api/v1/transfers")
   .middleware("auth");
 
 Route.group(() => {
-  Route.get("/get-by-id/:id","FunctionalProjectsController.getFunctionalProjectById");
-  Route.post("/get-paginated","FunctionalProjectsController.getFunctionalProjectPaginated");
+  Route.get(
+    "/get-by-id/:id",
+    "FunctionalProjectsController.getFunctionalProjectById"
+  );
+  Route.post(
+    "/get-paginated",
+    "FunctionalProjectsController.getFunctionalProjectPaginated"
+  );
   Route.post("/create", "FunctionalProjectsController.createFunctionalProject");
-  Route.post("/update-save/:id","FunctionalProjectsController.updateFunctionalProject");
-}).prefix("/api/v1/projectOperation")
+  Route.post(
+    "/update-save/:id",
+    "FunctionalProjectsController.updateFunctionalProject"
+  );
+})
+  .prefix("/api/v1/projectOperation")
   .middleware("auth");
 
 Route.group(() => {
@@ -197,7 +296,9 @@ Route.group(() => {
   Route.post("/edit-pac", "PacsController.editPac");
   Route.get("/get-pac-by-id/:id", "PacsController.getPacById");
   Route.post("/get-view-pac", "PacsController.viewPacComplete");
-}).prefix("/api/v1/pac");
+})
+  .prefix("/api/v1/pac")
+  .middleware("auth");
 
 Route.group(() => {
   Route.post(
@@ -231,7 +332,9 @@ Route.group(() => {
     "BudgetAvailabilityController.updateRoutesCDP"
   );
   Route.get("/get-CDPRp-id/:id", "BudgetAvailabilityController.getRpCDP");
-}).prefix("/api/v1/cdp");
+})
+  .prefix("/api/v1/cdp")
+  .middleware("auth");
 
 Route.group(() => {
   Route.post("/create-rp", "BudgetRecordsController.createRp");
@@ -246,7 +349,9 @@ Route.group(() => {
     "BudgetRecordsController.getTotalValuesImports"
   );
   Route.post("/update-data/:id", "BudgetRecordsController.updateRp");
-}).prefix("/api/v1/budget-records");
+})
+  .prefix("/api/v1/budget-records")
+  .middleware("auth");
 
 Route.group(() => {
   Route.post(
