4e46d633bfa5c94e768051802a627c85b924da8f
leiner mena <lmena@i4digital.com>
2023-12-15 10:01:06 -0500

    Carga masiva pospre ajustes

--- app/Services/PospreUploadMasiveService.ts
+++ app/Services/PospreUploadMasiveService.ts
@@ -1,3 +1,4 @@
+import { IPospreUploadMasive,IResponsePospreUploadMasive } from './../Interfaces/PosPreSapienciaInterfaces';
 import { IFunctionalAreaMasiveSave, IResponseUploadMasiveFunctionalArea } from './../Interfaces/FunctionalAreaInterfaces';
 import { DateTime } from 'luxon';
 import * as fs from 'fs';
@@ -8,41 +9,33 @@ import { ApiResponse } from '../Utils/ApiResponses';
 import { EResponseCodes } from '../Constants/ResponseCodesEnum';
 import FunctionalArea from 'App/Models/FunctionalArea';
 import ProjectsVinculation from 'App/Models/ProjectsVinculation';
-
-export interface IFunctionalAreaUploadService {
+import PosPreSapiencia from 'App/Models/PosPreSapiencia';
+export interface IPospreUploadMasiveService {
   uploadMasiveAreaFunctional(fileData: any, usuarioCreo: any, aditionalData: []): Promise<ApiResponse<any>>;
 }
-export default class FunctionalAreaUploadMasiveService implements IFunctionalAreaUploadService {
+export default class PospreUploadMasiveService implements IPospreUploadMasiveService {
   public async uploadMasiveAreaFunctional(fileData: any, usuarioCreo: any, aditionalData: []): Promise<ApiResponse<any>> {
     const result = await this.processBase64(fileData);
     let responseData;
 
     if (Object.keys(result).length > 0) {
       const items = result?.data?.items;
+      const aditionalDataItem = aditionalData;
+      const arrDataVpy = Object.values(aditionalDataItem)
 
       if (items && Array.isArray(items) && items.length > 0) {
         for (const [index, item] of items.entries()) {
-          const functionalArea: IFunctionalAreaMasiveSave = {
-            number: item.number,
-            userCreate: usuarioCreo,
-            denomination: "NO ESPECIFICA",
-            description: "No especifica",
-          }
-        
-          responseData = await this.insertItemsToDatabase([functionalArea], FunctionalArea);
-          const insertedIds = await this.insertItemsToDatabase([functionalArea], FunctionalArea);
-          const aditionalDataItem = aditionalData;
-          const arrDataVpy = Object.values(aditionalDataItem)
-          const vpyArrayInformation = {
-            functionalAreaId: insertedIds,
-            linked: true,
-            type: arrDataVpy[index]['tipoProyecto'] === 'funcionamiento' ? "Funcionamiento" : "Inversion",
-            operationProjectId: arrDataVpy[index]['tipoProyecto'] === 'funcionamiento' ? arrDataVpy[index]['id'] : null,
-            investmentProjectId: arrDataVpy[index]['tipoProyecto'] === 'inversion' ? arrDataVpy[index]['id'] : null,
+          const pospreSapiencia: IPospreUploadMasive = {
+            number: item.number+"12",
             userCreate: usuarioCreo,
             dateCreate: DateTime.fromJSDate(new Date()),
-          } 
-          await this.insertItemsToDatabaseVPY([vpyArrayInformation], ProjectsVinculation);
+            consecutive: "0",
+            assignedTo: item.number,
+            budgetId: arrDataVpy[index]['id']
+          }
+          await this.insertItemsToDatabaseVPY([pospreSapiencia], PosPreSapiencia);
+        
+        
         }
       } else {
         console.error('La propiedad "items" no es un arreglo válido o está vacía.');
@@ -54,7 +47,7 @@ export default class FunctionalAreaUploadMasiveService implements IFunctionalAre
     return new ApiResponse(responseData, EResponseCodes.OK, 'Áreas funcionales cargadas correctamente!');
   }
 
-  async processBase64(fileContent: string): Promise<ApiResponse<IResponseUploadMasiveFunctionalArea | null>> {
+  async processBase64(fileContent: string): Promise<ApiResponse<any | null>> {
     const fileBuffer = Buffer.from(fileContent, 'base64');
     const fileExtension = mimeTypes.extension('application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
     const tempFilePath = path.join(__dirname, '/files/uploads', `tempFile.${fileExtension}`);
@@ -71,12 +64,12 @@ export default class FunctionalAreaUploadMasiveService implements IFunctionalAre
     return readInfo;
   }
 
-  public readExcel = async (fileBuffer: Buffer): Promise<ApiResponse<IResponseUploadMasiveFunctionalArea | null>> => {
+  public readExcel = async (fileBuffer: Buffer): Promise<ApiResponse<any | null>> => {
     const workbook = new ExcelJS.Workbook();
     await workbook.xlsx.load(fileBuffer);
 
     const worksheet = workbook.getWorksheet(1);
-    const items: IFunctionalAreaMasiveSave[] = [];
+    const items: IPospreUploadMasive[] = [];
     const headers: string[] = [];
 
     worksheet.getRow(1).eachCell({ includeEmpty: true }, (cell) => {
@@ -86,12 +79,11 @@ export default class FunctionalAreaUploadMasiveService implements IFunctionalAre
     worksheet.eachRow((row, rowIndex) => {
       if (rowIndex === 1) return;
 
-      const objResult: IFunctionalAreaMasiveSave = {
+      const objResult: IPospreUploadMasive = {
         number: row.getCell(1).value?.toString()!,
-        userCreate: row.getCell(2).value?.toString()!,
-        denomination: row.getCell(3).value?.toString()!,
-        description: row.getCell(4).value?.toString()!,
-        dateCreate: DateTime.fromJSDate(new Date()),
+        consecutive: row.getCell(4).value?.toString()!,
+        ejercise: parseInt(String(row.getCell(5).value) || '0', 10),
+        description: row.getCell(6).value?.toString()!,
       };
 
       if (
@@ -103,7 +95,7 @@ export default class FunctionalAreaUploadMasiveService implements IFunctionalAre
       }
     });
 
-    const response: IResponseUploadMasiveFunctionalArea = {
+    const response: IResponsePospreUploadMasive = {
       generalResponse: "Los filtros se superaron exitosamente",
       errorsResponse: null,
       headers,
@@ -117,38 +109,7 @@ export default class FunctionalAreaUploadMasiveService implements IFunctionalAre
     );
   };
 
-  private async insertItemsToDatabase(
-    items: IFunctionalAreaMasiveSave[],
-    model: any
-  ): Promise<number> {
-    let insertedIds: number = 0;
 
-    for (const item of items) {
-      try {
-        const { number } = item;
-
-        const existingRecord = await model.query()
-          .where('ARF_CODIGO_REFERENCIA', number)
-          .first();
-
-        let insertedId;
-
-        if (existingRecord) {
-          await existingRecord.merge(item).save();
-          insertedId = existingRecord.id;
-        } else {
-          const newRecord = await model.create(item);
-          insertedId = newRecord.id;
-        }
-
-        insertedIds = parseInt(insertedId);
-      } catch (error) {
-        console.error(`Error de validación para el item: ${error}`);
-      }
-    }
-
-    return insertedIds;
-  }
 
   private async insertItemsToDatabaseVPY(
     items: any[],
