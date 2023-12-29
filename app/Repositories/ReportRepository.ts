d3a371dfd1a566b7bd9877570aac006585a8d2ac
Juan David Garcia <juan.david.garcia.mejia84@gmail.com>
2023-12-28 20:31:26 -0500

    Se ajusta validación en edición de rp

--- app/Repositories/ReportRepository.ts
+++ app/Repositories/ReportRepository.ts
@@ -67,7 +67,7 @@ export interface IReportRepository {
   generateReportCdpRpPayMga(year: number): Promise<any[]>;
 }
 
-interface ObjectFinaldata {
+/* interface ObjectFinaldata {
   Div: string;
   DateDocumentCdp: string;
   ContractualObject: string;
@@ -110,7 +110,7 @@ interface ObjectFinaldata {
   Cdp: number;
   Rp: number;
   NumberSapCdp: number;
-}
+} */
 
 
 
